aff3e6338081621ef41cf6a7fde52f46b8582f0c
dhimasajie13 <dhimasajie20@gmail.com>
2020-07-15 00:02:31 +0700

    first commit

---
+++ model/Order.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const objectId = mongoose.Schema.ObjectId
+
+const OrderSchema = new Schema({
+    idUser: {
+        type: objectId
+    },
+    idFilm: {
+        type: objectId
+    },
+    jumlah: {
+        type: Number
+    },
+    total: {
+        type: Number
+    },
+    image: {
+        type: String
+    },
+    // 1= belum di verifikasi, 2= sedang dalam pengiriman, 3=sudah diterima
+    status: {
+        type: Number,
+        default: 1
+    }
+})
+
+module.exports = mongoose.model('order', OrderSchema)
